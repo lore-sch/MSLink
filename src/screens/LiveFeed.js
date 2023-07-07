5c6db16571d97a444dc12800833d40e5b8674a90
Laura <107929355+lore-sch@users.noreply.github.com>
2023-07-07 13:19:31 +0100

    Live Feed status post bar

--- src/screens/LiveFeed.js
+++ src/screens/LiveFeed.js
@@ -1,23 +1,83 @@
-import React from 'react'
-import { View, Text, StyleSheet } from 'react-native'
+import React from 'react';
+import { View, Text, StyleSheet, TextInput, TouchableOpacity } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
+import { FontAwesome } from '@expo/vector-icons';
+import { AntDesign } from '@expo/vector-icons';
 
 const LiveFeed = () => {
+  const handleButtonPress = () => {
+    // Handle button press action here
+  };
+
   return (
     <View style={styles.container}>
-      <Text style={styles.textStyle}>Live Feed here</Text>
+      <View style={styles.statusInputContainer}>
+        <TextInput
+          style={styles.statusInput}
+          placeholder="Share with us..."
+          multiline={true}
+        />
+      </View>
+      <View style={styles.optionContainer}>
+        <Text style={styles.optionText}>Status</Text>
+        <Ionicons name="chatbox-outline" size={20} color="deepskyblue" />
+        <Text style={styles.optionText}>Photo</Text>
+        <FontAwesome name="photo" size={20} color="deepskyblue" />
+        <Text style={styles.optionText}>Poll</Text>
+        <AntDesign name="barschart" size={20} color="deepskyblue" />
+        <View style={styles.buttonContainer}>
+          <TouchableOpacity style={styles.button} onPress={handleButtonPress}>
+            <Text style={styles.buttonText}>Post</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
     </View>
-  )
-}
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
     alignItems: 'center',
     justifyContent: 'center',
-    marginTop: 300,
   },
-  textStyle: {
+  statusInputContainer: {
+    marginTop: 30,
+    width: 350,
+    borderBottomWidth: 0.5,
+    borderColor: '#afbebf',
+  },
+  statusInput: {
+    marginBottom: 15,
+    lineHeight: 24,
+    fontSize: 16,
+  },
+  optionContainer: {
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    paddingHorizontal: 8,
+    marginBottom: 10,
+    marginTop: 5,
+  },
+  optionText: {
+    paddingHorizontal: 20,
     color: 'deepskyblue',
-    fontSize: 30,
   },
-})
-export default LiveFeed
+  buttonContainer: {
+    flex: 1,
+    alignItems: 'flex-end',
+    paddingHorizontal: 10,
+  },
+  button: {
+    backgroundColor: 'deepskyblue',
+    paddingHorizontal: 16,
+    paddingVertical: 8,
+    borderRadius: 5,
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 16,
+  },
+});
+
+export default LiveFeed;
--- src/screens/SignUp.js
+++ src/screens/SignUp.js
@@ -1,5 +1,5 @@
 //Signup modal accessible from home page
-import { useState } from "react";
+import { useState } from 'react'
 import {
   View,
   Text,
@@ -8,110 +8,105 @@ import {
   TextInput,
   SafeAreaView,
   Modal,
-  Platform
-} from "react-native";
-import axios from "axios";
+  Platform,
+} from 'react-native'
+import axios from 'axios'
 
 //button styling and set up
 const SquareButton = ({ title, onPress }) => (
   <TouchableOpacity style={styles.button} onPress={onPress}>
     <Text style={styles.buttonText}>{title}</Text>
   </TouchableOpacity>
-);
+)
 
 //modal to handle entered email address, password
 //TO DO: Verify passwords match- error message displayed, but set min/max length?
 
 const SignUp = ({ showModal, setShowModal }) => {
-  const [enteredEmailAddress, setEnteredEmailAddress] = useState("");
-  const [enteredPassword, setEnteredPassword] = useState("");
+  const [enteredEmailAddress, setEnteredEmailAddress] = useState('')
+  const [enteredPassword, setEnteredPassword] = useState('')
   const [enteredPasswordConfirmation, setEnteredPasswordConfirmation] =
-    useState("");
-  const [passwordMatchError, setpasswordMatchError] = useState(false);
+    useState('')
+  const [passwordMatchError, setpasswordMatchError] = useState(false)
 
   const emailHandler = (enteredEmailAddress) => {
-    setEnteredEmailAddress(enteredEmailAddress);
-  };
+    setEnteredEmailAddress(enteredEmailAddress)
+  }
 
   const passwordHandler = (enteredPassword) => {
-    setEnteredPassword(enteredPassword);
-  };
+    setEnteredPassword(enteredPassword)
+  }
 
   const passwordConfirmationHandler = (enteredPasswordConfirmation) => {
-    setEnteredPasswordConfirmation(enteredPasswordConfirmation);
-  };
+    setEnteredPasswordConfirmation(enteredPasswordConfirmation)
+  }
 
   const signUpHandler = async () => {
     if (enteredPassword !== enteredPasswordConfirmation) {
-      setpasswordMatchError(true);
-      return;
+      setpasswordMatchError(true)
+      return
     }
-    let apiUrl = "http://localhost:3000/SignUp"; // Default API URL for iOS
-  
-    if (Platform.OS === "android") {
-      apiUrl = "http://10.0.2.2:3000/SignUp"; // Override API URL for Android
+    let apiUrl = 'http://localhost:3000/SignUp' // Default API URL for iOS
+
+    if (Platform.OS === 'android') {
+      apiUrl = 'http://10.0.2.2:3000/SignUp' // Override API URL for Android
     }
-  
+
     try {
       const response = await axios.post(apiUrl, {
         userEmail: enteredEmailAddress,
         userPassword: enteredPasswordConfirmation,
-      });
+      })
       //resets text input fields to empty
-      setEnteredEmailAddress("");
-      setEnteredPassword("")
-  
-      
-    } catch (error) {
-    
-    }
-  };
+      setEnteredEmailAddress('')
+      setEnteredPassword('')
+    } catch (error) {}
+  }
 
   return (
-    <Modal visible={showModal} animationType="slide">
+    <Modal visible={showModal} animationType='slide'>
       <SafeAreaView>
         <View style={styles.inputContainer}>
           <Text style={styles.inputPrompt}>Email Address:</Text>
           <TextInput
             style={styles.textInput}
-            placeholder="example@example.co.uk"
+            placeholder='example@example.co.uk'
             onChangeText={emailHandler}
           />
           <Text style={styles.inputPrompt}>Password:</Text>
           <TextInput
             style={styles.textInput}
-            placeholder="Enter your password"
+            placeholder='Enter your password'
             onChangeText={passwordHandler}
             secureTextEntry={true}
           />
           <Text style={styles.inputPrompt}>Confirm Password:</Text>
           <TextInput
             style={styles.textInput}
-            placeholder="Re-enter your password"
+            placeholder='Re-enter your password'
             onChangeText={passwordConfirmationHandler}
             secureTextEntry={true}
           />
-           {passwordMatchError && (
-              <Text style={styles.errorMessage}>
-                Passwords do not match. Please try again.
-              </Text>
-            )}
+          {passwordMatchError && (
+            <Text style={styles.errorMessage}>
+              Passwords do not match. Please try again.
+            </Text>
+          )}
           <View style={styles.buttonContainer}>
-            <SquareButton title="Sign Up" onPress={signUpHandler} />
-            <SquareButton title="Cancel" onPress={() => setShowModal(false)} />
-           
+            <SquareButton title='Sign Up' onPress={signUpHandler} />
+            <SquareButton title='Cancel' onPress={() => setShowModal(false)} />
           </View>
         </View>
       </SafeAreaView>
     </Modal>
-  );
-};
+  )
+}
 
 const styles = StyleSheet.create({
   inputContainer: {
-    flexDirection: "column",
-    justifyContent: "center",
-    alignItems: "center",
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
     padding: 16,
     marginTop: 200,
   },
@@ -120,34 +115,34 @@ const styles = StyleSheet.create({
   },
   textInput: {
     borderWidth: 1,
-    borderColor: "black",
+    borderColor: 'black',
     borderRadius: 6,
-    width: "80%",
+    width: '80%',
     padding: 8,
     margin: 15,
   },
   buttonContainer: {
-    flexDirection: "row",
-    justifyContent: "center",
-    alignItems: "center",
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
     marginTop: 20,
   },
   button: {
-    backgroundColor: "deepskyblue",
+    backgroundColor: 'deepskyblue',
     margin: 15,
     width: 100,
     padding: 10,
-    justifyContent: "center",
-    alignItems: "center",
+    justifyContent: 'center',
+    alignItems: 'center',
     borderRadius: 10,
   },
   buttonText: {
     fontSize: 18,
-    color: "white",
+    color: 'white',
   },
   errorMessage: {
-    color: 'red'
-  }
-});
+    color: 'red',
+  },
+})
 
-export default SignUp;
+export default SignUp
